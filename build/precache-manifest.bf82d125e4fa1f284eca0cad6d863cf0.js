3b0afffa75b11ae0d56441fe030aab1a024a5191
Shubham Tiwari <shubht3303@gmail.com>
2025-04-04 14:30:46 +0530

    ok

---
+++ build/precache-manifest.bf82d125e4fa1f284eca0cad6d863cf0.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "631aa5c22f02854d82a8fe0801f1dc3f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b833e8a19181806f7bc6",
+    "url": "/static/css/2.6620286d.chunk.css"
+  },
+  {
+    "revision": "b833e8a19181806f7bc6",
+    "url": "/static/js/2.088fae7c.chunk.js"
+  },
+  {
+    "revision": "9601d2d1db0643a07bc509e8b4392d79",
+    "url": "/static/js/2.088fae7c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6985813512098e3ac462",
+    "url": "/static/js/main.b2837404.chunk.js"
+  },
+  {
+    "revision": "c7805d0a662790c235f8",
+    "url": "/static/js/runtime-main.c0105f97.js"
+  },
+  {
+    "revision": "5fc0c9bdb40040928ed73dbbcecd62a3",
+    "url": "/static/media/eth.5fc0c9bd.png"
+  },
+  {
+    "revision": "6dca538af9a96d32bb52d2f64a4b6f9a",
+    "url": "/static/media/logo_white.6dca538a.svg"
+  },
+  {
+    "revision": "cad8371a1104644d48ca271025e96ea8",
+    "url": "/static/media/oboswap.cad8371a.svg"
+  },
+  {
+    "revision": "deb6601724fb63039ab37e26448a710c",
+    "url": "/static/media/wordmark.deb66017.svg"
+  }
+]);
